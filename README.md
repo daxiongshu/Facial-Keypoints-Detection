06b719da8a2e2c63a3818f90dbdd24cb61a1bd14
daxiongshu <aixueer4ever@gmail.com>
2015-08-16 12:41:24 -0400

    new code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Facial-Keypoints-Detection
-# 
+This is the working directory for the team Deep Learning Freelancers for the kaggle contest Facial Keypoints Detection
+https://www.kaggle.com/c/facial-keypoints-detection
+
---
+++ carl/run.sh
@@ -0,0 +1,20 @@
+#To use this script, first run this to fit your first model:
+python kfkd_v1.py fit
+
+#Then train a bunch of specialists that intiliaze their weights from
+#your first model:
+#modified by carl
+#train one 'column' at a time
+
+python kfkd_v1.py fit_specialists net.pickle 0
+python kfkd_v1.py fit_specialists net.pickle 1
+python kfkd_v1.py fit_specialists net.pickle 2
+python kfkd_v1.py fit_specialists net.pickle 3
+python kfkd_v1.py fit_specialists net.pickle 4
+python kfkd_v1.py fit_specialists net.pickle 5
+
+#Plot their error curves:
+python kfkd_v1.py plot_learning_curves net-specialists_5.pickle
+#And finally make predictions to submit to Kaggle:
+python kfkd_v1.py predict net-specialists_5.pickle
+
